ef4c3eb1f362016680b1d8c77c4aee27748f9598
Joovzx <suryaniwati07@gmail.com>
2025-05-27 12:41:34 +0700

    Praktikum Pekan 6

---
+++ src/pekan6/NodeDLL.java
@@ -0,0 +1,16 @@
+package pekan6;
+
+public class NodeDLL {
+	int data;
+	NodeDLL next;
+	NodeDLL prev;
+	
+	public NodeDLL(int data) {
+		this.data = data;
+		this.next = null;
+		this.prev = null;
+	}
+	
+	// Jovantri Immanuel Gulo
+	// 2411532014
+}
